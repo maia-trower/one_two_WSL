b09a3129713aae4a0312e3fc239a9f669977cfa3
maia-trower <83399894+maia-trower@users.noreply.github.com>
2023-06-02 18:11:52 +0100

    script to plot one-two passes for given season (per match)

--- one_two_season_plots.py
+++ one_two_season_plots.py
@@ -3,9 +3,9 @@ import pickle
 import json
 
 
-def main(country):
+def main(input_file):
     # load in dictionary of inputs here
-    with open(f"inputs/{country}_euros.json", "r") as f:
+    with open(f"inputs/{input_file}.json", "r") as f:
         inputs = json.load(f)
 
     comp = inputs["competition_id"]
@@ -17,20 +17,20 @@ def main(country):
     c_thresh = inputs["threshold_carry"]
     figpath = inputs["fig_path"]
     save_data = inputs["data_save_path"]
+    to_save = bool(inputs["to_save"])
 
     all_matches = get_pass_data(competition_ID=comp, season_ID=season, team=team, data_path=path)
 
-    # save dictionary as pickle file
     with open(f"{save_data}{comp}_{season}_{team}.pickle", "wb") as f:
         pickle.dump(all_matches, f, protocol=pickle.HIGHEST_PROTOCOL)
 
+    # change this now!!!!
     for key in all_matches.keys():
-        print(key)
         one_twos = get_one_twos(match=all_matches[key], sec_threshold=sec_thresh, prog_threshold=p_thresh, carry_threshold=c_thresh)
 
         plot_path = figpath + f"{comp}_{season}_{team}_{key}.png"
-        plot_match_one_twos(data=one_twos, title="", save_path=plot_path, save=True, grid=False)
+        plot_match_one_twos(data=one_twos, save_path=plot_path, save=to_save, grid=False)
 
 
 if __name__ == '__main__':
-    main("germany")
+    main("arsenal_WSL_19")
