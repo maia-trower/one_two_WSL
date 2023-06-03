22417ed66a20eb7421b394f82b5deaaf79cc609a
maia-trower <83399894+maia-trower@users.noreply.github.com>
2023-06-03 12:30:45 +0100

    configure input file for plot scripts

--- final_code/generate_plots.py
+++ final_code/generate_plots.py
@@ -1,25 +1,32 @@
 # generate all plots for presentation
 from one_two.functions import get_player_one_twos, plot_one_two_heatmaps, get_season_one_twos, get_player_counts, \
     total_one_two_stack, key_pass_relplot
+import json
 
 # TODO set params fontsize etc
 
-
-if __name__ == '__main__':
+def main(input_file):
     # read in inputs
+    with open(f"inputs/{input_file}.json", "r") as f:
+        inputs = json.load(f)
     comp = inputs["competition_name"]
     comp_id = inputs["competition_id"]
     season_id = inputs["season_id"]
     players = inputs["players"]
     n = inputs["n"]
+    s_thresh = inputs["threshold_seconds"]
+    p_thresh = inputs["threshold_progression"]
+    c_thresh = inputs["threshold_carry"]
 
     # get data here
-    one_two_agg = get_season_one_twos(comp=comp_id, season=season_id)
-    open_player_counts, close_player_counts = get_player_counts()
+    one_two_agg = get_season_one_twos(comp=comp_id, season=season_id, path="", s=s_thresh, p=p_thresh, c=c_thresh)
+    open_player_counts, close_player_counts = get_player_counts(one_two_agg)
     # combine open and close for stacked plot
     one_two_player_counts = open_player_counts.merge(close_player_counts, on="player", how="outer").rename(
         columns={"count_x": "open_count", "count_y": "close_count", "team_x": "team"})
     one_two_player_counts = one_two_player_counts.drop(columns=["team_y"])
+    # add a total count col
+    one_two_player_counts["total_count"] = one_two_player_counts["open_count"] + one_two_player_counts["close_count"]
 
     # then do the plots in order
     # 1. stacked bar plot
@@ -30,10 +37,12 @@ if __name__ == '__main__':
 
     # 3. heatmap plot
     # do heatmap for each player
-    player_data={}
+    player_data = {}
     for player in players:
         player_data[player] = get_player_one_twos(player, one_two_agg)
-        plot_one_two_heatmaps(data=player_data[player], competition=comp, season=season_id)
+        plot_one_two_heatmaps(data=player_data[player], competition=comp, season=season_id, team=player)
 
 
+if __name__ == '__main__':
+    main(input_file="input_v1")
 
